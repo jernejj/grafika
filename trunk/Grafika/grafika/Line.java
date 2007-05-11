c8a2d468ac89046df2c1cafd610fc8c4c6250bc1
Samo.Sela <Samo.Sela@4b4ad2c3-c42c-0410-bba1-47d73fdcdd88>
2007-05-11 15:43:12 +0000

    Small changes.

--- trunk/Grafika/grafika/Line.java
+++ trunk/Grafika/grafika/Line.java
@@ -1,11 +1,44 @@
 package grafika;
 
+import java.awt.Point;
+
 public class Line {
 	Element oneEnd;
 	Element otherEnd;
 	
+	private Point startPoint;
+	private Point endPoint;
+	
+	public Line() {
+		
+	}
+	
 	public Line(Element first, Element second) {
 		this.oneEnd = first;
 		this.otherEnd = second;
+	}
+	
+	public void setOneEndElement(Element e) {
+		this.oneEnd = e;
+	}
+	
+	public void setOtherEndElement(Element e) {
+		this.otherEnd = e;
+	}
+	
+	public Point getStartPoint() {
+		return this.startPoint;
+	}
+	
+	public void setStartPoint(Point starting_position) {
+		this.startPoint = starting_position;
+	}
+	
+	public Point getEndPoint() {
+		return this.endPoint;
+	}
+	
+	public void setEndPoint(Point ending_position) {
+		this.endPoint = ending_position;
 	}
 }
