66acd801ebc327a274141a0aba261a17dadcd379
Samo.Sela <Samo.Sela@4b4ad2c3-c42c-0410-bba1-47d73fdcdd88>
2007-05-11 02:06:39 +0000

    New class for computation.

---
+++ trunk/Grafika/grafika/Pin.java
@@ -0,0 +1,56 @@
+package grafika;
+
+import java.awt.Point;
+
+public class Pin {
+	private int value;
+	private Point up;
+	private Point down;
+	private int type;
+	
+	public final static int IN = 1;
+	public final static int OUT = 2;
+	
+	public Pin(int type) {
+		
+	}
+	
+	public int getValue() {
+		return this.value;
+	}
+	
+	public void setValue(int value) {
+		this.value = value & 0x01;
+	}
+	
+	public Point getUp() {
+		return this.up;
+	}
+	
+	public void setUp(Point up) {
+		this.up = up;
+	}
+
+	public Point getDown() {
+		return this.down;
+	}
+	
+	public void setDown(Point down) {
+		this.down = down;
+	}
+	
+	private void setType(int type) {
+		switch(type) {
+			case IN:
+				this.type = IN;
+				break;
+			case OUT:
+				this.type = OUT;
+				break;
+			default:
+				System.err.println("Wrong pin type!");
+				System.exit(101);
+				break;
+		}
+	}
+}
