82eafe0c58374ae22e142929f91060326d4de7bf
simon.striker <simon.striker@4b4ad2c3-c42c-0410-bba1-47d73fdcdd88>
2007-05-12 14:13:34 +0000

    Elements GND and VCC added.

--- trunk/Grafika/grafika/DocText.java
+++ trunk/Grafika/grafika/DocText.java
@@ -10,7 +10,9 @@ class DocText extends TextArea {
 	final String NOR = "Operator NOR selected.";
 	final String XOR = "Operator XOR selected.";
 	final String XNOR = "Operator XNOR selected.";
-	final String NOT = "Operator NOT selected."; 
+	final String NOT = "Operator NOT selected.";
+	final String GND = "Operator GND selected."; 
+	final String VCC = "Operator VCC selected."; 
 	final String BOX = "Nothing to do!";
 	final String LOCKED = "Selection not possible!";
 	final String IZBERI_ELEMENT = "Izberi element!";
@@ -32,6 +34,8 @@ class DocText extends TextArea {
 		else if (str.equals("XOR")) setText(XOR);
 		else if (str.equals("XNOR")) setText(XNOR);
 		else if (str.equals("NOT")) setText(NOT);
+		else if (str.equals("GND")) setText(GND);
+		else if (str.equals("VCC")) setText(VCC);
 		else if (str.equals("BOX")) setText(BOX);
 		else if (str.equals("LOCKED")) setText(LOCKED);
 		else if (str.equals("IZBERI_ELEMENT")) setText(IZBERI_ELEMENT);
