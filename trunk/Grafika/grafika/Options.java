a6e010638c29470c858dee62a9cc4d41d909a426
Samo.Sela <Samo.Sela@4b4ad2c3-c42c-0410-bba1-47d73fdcdd88>
2007-05-12 15:56:40 +0000

    Removed unused imports.

--- trunk/Grafika/grafika/Options.java
+++ trunk/Grafika/grafika/Options.java
@@ -1,12 +1,9 @@
 package grafika;
 
 import java.awt.Color;
-import java.awt.Event;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.GridLayout;
 import java.awt.Insets;
-import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
