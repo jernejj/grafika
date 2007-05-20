3e09b6892aeb73b7149bb97275203d0d48a82341
simon.striker <simon.striker@4b4ad2c3-c42c-0410-bba1-47d73fdcdd88>
2007-05-20 05:13:18 +0000

    Load and Save functionality added.

--- Grafika/grafika/Xml.java
+++ Grafika/grafika/Xml.java
@@ -1,11 +1,25 @@
 package grafika;
 
 import java.awt.FileDialog;
-import java.io.*;
+import java.awt.Point;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.util.Iterator;
 import java.util.Vector;
 
 import javax.swing.JFrame;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
 
 public class Xml
 {	
@@ -99,6 +113,217 @@ public class Xml
 	
 	public static void load (Grafika parent)
 	{
+		JFrame frame = new JFrame();
 		
+		FileDialog d = new FileDialog(frame, "Load", FileDialog.LOAD);
+		d.setVisible(true);
+		
+		String file = d.getFile();
+		
+		if(file != null)
+		{
+			if (!file.endsWith(".xml"))
+				parent.documentation.doctext.showline("LOAD_FAILED");
+			
+			else
+			{
+				file = d.getDirectory()+file;
+				try
+				{
+		            DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+		            DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+		            Document doc = docBuilder.parse(new File(file));
+
+		            // Normalize text representation
+		            doc.getDocumentElement().normalize();
+		            
+		            if (!(doc.getDocumentElement().getNodeName().equals("GrafikaCanvas")))
+		            	parent.documentation.doctext.showline("LOAD_INVALID");
+		            
+		            else
+		            {
+			            NodeList numOfElements = doc.getElementsByTagName("NumberOfElements");
+			            org.w3c.dom.Element firstElNode = (org.w3c.dom.Element)numOfElements.item(0);
+			            NodeList firstEleNodeValue = firstElNode.getChildNodes();
+			            Grafika.element = Integer.parseInt(((Node)firstEleNodeValue.item(0)).getNodeValue().trim());
+			            
+			            NodeList listOfElements = doc.getElementsByTagName("Element");
+			            
+			            String name;
+	            		int type;
+	            		int posX;
+	            		int posY;
+	            		int pin1;
+	            		int pin2;
+	            		int out;
+			            
+			            // Sprehodimo se po vseh elementih
+			            for (int i=0; i<listOfElements.getLength(); i++)
+			            {	
+			            	Node firstElementNode = listOfElements.item(i);
+			            	if (firstElementNode.getNodeType() == Node.ELEMENT_NODE)
+			            	{
+			            		org.w3c.dom.Element firstElement = (org.w3c.dom.Element)firstElementNode;
+			            		
+			            		// Za Element ID
+			            		NodeList elementID = firstElement.getElementsByTagName("ID");
+			            		org.w3c.dom.Element elementIDelement = (org.w3c.dom.Element)elementID.item(0);
+			            		NodeList textElIDList = elementIDelement.getChildNodes();
+			            		name = ((Node)textElIDList.item(0)).getNodeValue().trim();
+			            		
+			            		// Za Element Type
+			            		NodeList elementType = firstElement.getElementsByTagName("Type");
+			            		org.w3c.dom.Element elementTypeElement = (org.w3c.dom.Element)elementType.item(0);
+			            		NodeList textElTypeList = elementTypeElement.getChildNodes();
+			            		type = Integer.parseInt(((Node)textElTypeList.item(0)).getNodeValue().trim());
+			            		
+			            		// Za Element PosX
+			            		NodeList elementPosX = firstElement.getElementsByTagName("PosX");
+			            		org.w3c.dom.Element elementPosXElement = (org.w3c.dom.Element)elementPosX.item(0);
+			            		NodeList textElPosXList = elementPosXElement.getChildNodes();
+			            		posX = Integer.parseInt(((Node)textElPosXList.item(0)).getNodeValue().trim());
+			            		
+			            		// Za Element PosY
+			            		NodeList elementPosY = firstElement.getElementsByTagName("PosY");
+			            		org.w3c.dom.Element elementPosYElement = (org.w3c.dom.Element)elementPosY.item(0);
+			            		NodeList textElPosYList = elementPosYElement.getChildNodes();
+			            		posY = Integer.parseInt(((Node)textElPosYList.item(0)).getNodeValue().trim());
+			            		
+			            		// Za Element Pin1
+			            		NodeList elementPin1 = firstElement.getElementsByTagName("Pin1");
+			            		org.w3c.dom.Element elementPin1Element = (org.w3c.dom.Element)elementPin1.item(0);
+			            		NodeList textElPin1List = elementPin1Element.getChildNodes();
+			            		pin1 = Integer.parseInt(((Node)textElPin1List.item(0)).getNodeValue().trim());
+			            		
+			            		// Za Element Pin2
+			            		NodeList elementPin2 = firstElement.getElementsByTagName("Pin2");
+			            		org.w3c.dom.Element elementPin2Element = (org.w3c.dom.Element)elementPin2.item(0);
+			            		NodeList textElPin2List = elementPin2Element.getChildNodes();
+			            		pin2 = Integer.parseInt(((Node)textElPin2List.item(0)).getNodeValue().trim());
+			            		
+			            		// Za Element Out
+			            		NodeList elementOut = firstElement.getElementsByTagName("Out");
+			            		org.w3c.dom.Element elementOutElement = (org.w3c.dom.Element)elementOut.item(0);
+			            		NodeList textElOutList = elementOutElement.getChildNodes();
+			            		out = Integer.parseInt(((Node)textElOutList.item(0)).getNodeValue().trim());
+			            		
+			            		Element element = new Element (parent, type, new Point (posX, posY));
+			            		element.setPin1value(pin1);
+			            		element.setPin2value(pin2);
+			            		element.setOut(out);
+			            		element.setName(name);
+			            		
+			            		parent.GrafikaCanvas.elementList.add(element);
+			            	}
+			            }
+			            NodeList listOfLines = doc.getElementsByTagName("Line");
+		            	
+		            	String lOut;
+		            	String lIn;
+		            	int lInPosX;
+		            	int lInPosY;
+		            	int lOutPosX;
+		            	int lOutPosY;
+		            	
+		            	// Sprehodimo se po vseh povezavah
+			            for (int i=0; i<listOfLines.getLength(); i++)
+			            {
+			            	Node firstLineNode = listOfLines.item(i);
+			            	if (firstLineNode.getNodeType() == Node.ELEMENT_NODE)
+			            	{
+			            		org.w3c.dom.Element firstLine = (org.w3c.dom.Element)firstLineNode;
+			            		
+			            		// Za Line Element Out
+			            		NodeList lineOut = firstLine.getElementsByTagName("Out");
+			            		org.w3c.dom.Element lineOutElement = (org.w3c.dom.Element)lineOut.item(0);
+			            		NodeList textLineOutList = lineOutElement.getChildNodes();
+			            		lOut = ((Node)textLineOutList.item(0)).getNodeValue().trim();
+			            		
+			            		// Za Line Element In
+			            		NodeList lineIn = firstLine.getElementsByTagName("In");
+			            		org.w3c.dom.Element lineInElement = (org.w3c.dom.Element)lineIn.item(0);
+			            		NodeList textLineInList = lineInElement.getChildNodes();
+			            		lIn = ((Node)textLineInList.item(0)).getNodeValue().trim();
+			            		
+			            		// Za Line In Point
+			            		NodeList LineInPoint = doc.getElementsByTagName("InPoint");
+			            		Node InPoint = LineInPoint.item(i);
+			            		org.w3c.dom.Element firstPoint = (org.w3c.dom.Element)InPoint;
+			            		
+			            		// Za Line In Point PosX
+			            		NodeList firstX = firstPoint.getElementsByTagName("PosX");
+			            		org.w3c.dom.Element firstXElement = (org.w3c.dom.Element)firstX.item(0);
+			            		NodeList textFirstXList = firstXElement.getChildNodes();
+			            		lInPosX = Integer.parseInt((((Node)textFirstXList.item(0)).getNodeValue().trim()));
+			            		
+			            		// Za Line In Point PosY
+			            		NodeList firstY = firstPoint.getElementsByTagName("PosY");
+			            		org.w3c.dom.Element firstYElement = (org.w3c.dom.Element)firstY.item(0);
+			            		NodeList textFirstYList = firstYElement.getChildNodes();
+			            		lInPosY = Integer.parseInt((((Node)textFirstYList.item(0)).getNodeValue().trim()));
+			            		
+			            		// Za Line Out Point
+			            		NodeList LineOutPoint = doc.getElementsByTagName("OutPoint");
+			            		Node OutPoint = LineOutPoint.item(i);
+			            		org.w3c.dom.Element secondPoint = (org.w3c.dom.Element)OutPoint;
+			            		
+			            		// Za Line Out Point PosX
+			            		NodeList secondX = secondPoint.getElementsByTagName("PosX");
+			            		org.w3c.dom.Element secondXElement = (org.w3c.dom.Element)secondX.item(0);
+			            		NodeList textSecondXList = secondXElement.getChildNodes();
+			            		lOutPosX = Integer.parseInt((((Node)textSecondXList.item(0)).getNodeValue().trim()));
+			            		
+			            		// Za Line Out Point PosY
+			            		NodeList secondY = secondPoint.getElementsByTagName("PosY");
+			            		org.w3c.dom.Element secondYElement = (org.w3c.dom.Element)secondY.item(0);
+			            		NodeList textSecondYList = secondYElement.getChildNodes();
+			            		lOutPosY = Integer.parseInt((((Node)textSecondYList.item(0)).getNodeValue().trim()));
+			            		
+			            		Vector<Element> elementList = parent.GrafikaCanvas.elementList;
+			    				Iterator<Element> ite = elementList.iterator();
+			    				
+			    				Element element;
+			    				Element lineElementOut = null;
+			    				Element lineElementIn = null;
+			    				
+			    				while (ite.hasNext())
+			    				{
+			    					element = ite.next();
+			    					if (element.getName().equals(lOut))
+			    						lineElementOut = element;
+			    					else if (element.getName().equals(lIn))
+			    						lineElementIn = element;
+			    				}
+			    				
+			    				Line line = new Line (lineElementOut, lineElementIn);
+			    				line.setOutPoint(new Point(lOutPosX, lOutPosY));
+			    				line.setInPoint(new Point (lInPosX, lInPosY));
+			    				
+			    				parent.GrafikaCanvas.lineList.add(line);
+			            	}
+			            }
+			            
+		            	parent.documentation.doctext.showline("LOADED");
+		            }
+		        }
+				catch (SAXParseException err)
+				{
+			        System.out.println ("** Parsing error" + ", line " + err.getLineNumber () + ", uri " + err.getSystemId ());
+			        System.out.println(" " + err.getMessage ());
+		        }
+				catch (SAXException e)
+		        {
+			        Exception x = e.getException ();
+			        ((x == null) ? e : x).printStackTrace ();
+		        }
+				catch (Throwable t)
+				{
+		        	t.printStackTrace ();
+				}
+			}
+		}
+		
+		else
+			parent.documentation.doctext.showline("LOAD_CANCELLED");
 	}
 }
